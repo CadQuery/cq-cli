321a4e66f81655656efcd1a90d30278773bf775b
Jeremy Wright <wrightjmf@gmail.com>
2020-10-21 09:57:50 -0400

    Fixed a bug where the codec loader was searching in the pwd.

--- cqcodecs/loader.py
+++ cqcodecs/loader.py
@@ -6,7 +6,7 @@ def load_codecs():
     cq_codecs = {}
 
     # Search all of the modules in the current directory to find codecs
-    for finder, name, ispkg in pkgutil.iter_modules([os.path.join(".", "cqcodecs")]):
+    for finder, name, ispkg in pkgutil.iter_modules([os.path.dirname(__file__)]):
         if name.startswith('cq_codec_'):
             cq_codecs[name] = importlib.import_module("cqcodecs." + name)
 
