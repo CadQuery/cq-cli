ed960274abb5989ca7ca32b8c3075bfa2a83592c
Jeremy Wright <wrightjmf@gmail.com>
2021-04-15 09:56:33 -0400

    Added a test for the Three.js codec

---
+++ tests/test_threejs_codec.py
@@ -0,0 +1,13 @@
+import tests.test_helpers as helpers
+
+def test_threejs_codec():
+    """
+    Basic test of the TJS (Three.js) codec plugin.
+    """
+    test_file = helpers.get_test_file_location("cube.py")
+
+    command = ["python", "cq-cli.py", "--codec", "threejs", "--infile", test_file]
+    out, err, exitcode = helpers.cli_call(command)
+
+    assert out.decode().split('\n')[5].replace('\r', '') == '        "vertices"      : 24,'
+    assert out.decode().split('\n')[6].replace('\r', '') == '        "faces"         : 12,'
