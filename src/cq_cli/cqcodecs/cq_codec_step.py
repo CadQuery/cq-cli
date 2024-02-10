652c3e1f232faa9179b8647ef6c8766a06729a08
Andreas Kahler <mail@andreaskahler.com>
2024-02-10 17:04:22 +0100

    use Assembly.save() for saving assemblies to step

--- src/cq_cli/cqcodecs/cq_codec_step.py
+++ src/cq_cli/cqcodecs/cq_codec_step.py
@@ -12,16 +12,15 @@ def convert(build_result, output_file=None, error_file=None, output_opts=None):
     # The exporters will add extra output that we do not want, so suppress it
     with helpers.suppress_stdout_stderr():
         # There should be a shape in the build results
-        result = build_result.results[0].shape
+        shape = build_result.results[0].shape
 
-        # If the build result is an assembly, we have to make it a compound before exporting
-        if type(result).__name__ == "Assembly":
-            result = result.toCompound()
-
-        # Put the STEP output into the temp file
-        exporters.export(
-            result, temp_file, exporters.ExportTypes.STEP
-        )
+        # assembly or a single object?
+        if type(shape).__name__ == "Assembly":
+            # use assembly save method
+            shape.save(temp_file)
+        else:
+            # Put the STEP output into the temp file
+            exporters.export(shape, temp_file, exporters.ExportTypes.STEP)
 
     # Read the STEP output back in
     with open(temp_file, "r") as file:
